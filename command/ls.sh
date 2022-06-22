1b241186df7a4546b03b80587a02a292caa5d6b2
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-23 05:11:27 +0900

    Update ls.sh

--- command/ls.sh
+++ command/ls.sh
@@ -1,3 +1,9 @@
 ls // list
 
 ls -l filename.txt //gen data, modify list
+ls -a //view all file and hidden file
+ls -c //view all file and sort for edit time
+ls -d //show dir
+ls -t//sort for time
+ls -s//file size (1Kb)
+ls -F//file form (excute : *, dir : /,symbol : @, FIFO :|, socket : =)
