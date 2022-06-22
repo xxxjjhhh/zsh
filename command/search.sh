d5d46f6c47bd84a4278381f963f17983a985acfb
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-06-23 06:03:55 +0900

    Update search.sh

--- command/search.sh
+++ command/search.sh
@@ -4,3 +4,7 @@ locate : 파일 검색 DB dir에 있는것만
 which : 실행파일 검색 환경변수 PATH에 지정되어 있는 dir만
 whereis : which에서 source + menual 
 find : 범용
+
+find ex.
+find 현재디렉토리 옵션 조건
+find . -name "*.jpg" (. : 현재dir, -name : 옵션)
